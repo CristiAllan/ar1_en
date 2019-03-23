8bc072045b6f0770cf5ccad43f70bd152af69947
CristiAllan <cristiallan@gmail.com>
2019-03-23 16:32:48 -0300

    create transaction example

---
+++ app/services/transfer_service.rb
@@ -0,0 +1,18 @@
+class TransferService
+  def perform(user, company, amount_cents)
+    ActiveRecord::Base.transaction do
+      raise ActiveRecord::RecordInvalid unless amount_cents.is_a? Integer
+
+      user.update!(balance_cents: user.balance_cents + amount_cents)
+      company.update!(balance_cents: company.balance_cents - amount_cents)
+
+      Transfer.transaction do
+        Transfer.create!(balance_cents: amount_cents, user: user, company: company)
+      end
+    end
+  rescue ActiveRecord::RecordInvalid
+    puts 'Invalid amount_cents'
+  end
+end
+
+# TransferService.perform(user, company, 500)
